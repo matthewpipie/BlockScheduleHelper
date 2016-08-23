ff2e5c07013bf640f4bdbcf2f9aa17058cf2c772
matthewpipie <matthewpipie@gmail.com>
2016-08-22 22:01:47 -0500

    2.1.1 android post

--- platforms/android/assets/www/js/setUpStorage.js
+++ platforms/android/assets/www/js/setUpStorage.js
@@ -16,30 +16,4 @@
  *
  * Contact the creator (Matthew Giordano) at matthewpipie@gmail.com.
  */
-
- var setUpStorage = {
-
-	storage: false,
-
-	pagecontainerbeforeshow: function() {},
-
-	deviceready: function() {
-		localforage.defineDriver(window.cordovaSQLiteDriver).then(function() {
-			return localforage.setDriver([
-					// Try setting cordovaSQLiteDriver if available,
-				window.cordovaSQLiteDriver._driver,
-					// otherwise use one of the default localforage drivers as a fallback.
-					// This should allow you to transparently do your tests in a browser
-				localforage.INDEXEDDB,
-				localforage.WEBSQL,
-				localforage.LOCALSTORAGE
-			]);
-		}).then(function() {
-	  // this should alert "cordovaSQLiteDriver" when in an emulator or a device
-			//alert(localforage.driver());
-			storage = true;
-			cordova.plugins.notification.local.on('trigger', function(noti, str) {console.log("triggered :O"); console.log(str); setUpSettings.scheduleNextEventAndClear(noti, false)});
-			app.mainSetUp();
-		});
-	}
-};
+var setUpStorage={storage:!1,pagecontainerbeforeshow:function(){},deviceready:function(){localforage.defineDriver(window.cordovaSQLiteDriver).then(function(){return localforage.setDriver([window.cordovaSQLiteDriver._driver,localforage.INDEXEDDB,localforage.WEBSQL,localforage.LOCALSTORAGE])}).then(function(){storage=!0,cordova.plugins.notification.local.on("trigger",function(a,b){console.log("triggered :O"),console.log(b),setUpSettings.scheduleNextEventAndClear(a,!1)}),app.mainSetUp()})}};
